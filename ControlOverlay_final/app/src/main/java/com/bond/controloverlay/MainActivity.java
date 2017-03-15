26a40544548c1fe2b21b161cd813a9f0e4d34589
WoutersRaymond <r.m.c.m.wouters@tue.nl>
2017-03-15 09:48:24 +0100

    Final control overlay
    working control overlay with fancy main screen

---
+++ ControlOverlay_final/app/src/main/java/com/bond/controloverlay/MainActivity.java
@@ -0,0 +1,38 @@
+package com.bond.controloverlay;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        getRequestedOrientation();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+    }
+
+    /** Called when the user clicks the Engine Start button, opens the control window of the app!*/
+    public void sendStart(View view) {
+        Intent intent = new Intent(this, ControlActivity.class);        /**jumps to new activity**/
+        /**EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+         *
+         */
+        startActivity(intent);
+
+           }
+}
